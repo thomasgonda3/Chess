a876679557d85b43afd623ade24b5101725e2086
Thomas Gonda <passinto06@gmail.com>
2020-06-19 00:21:51 -0700

    changed readme file

---
+++ build/precache-manifest.8ca74e4313e33ddc2112ac4436b748c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7aaab3dbe55bec0e1016ad749c8d092",
+    "url": "/Chess-ThomasG/index.html"
+  },
+  {
+    "revision": "e136e7d520013b7c2481",
+    "url": "/Chess-ThomasG/static/css/main.a56693d4.chunk.css"
+  },
+  {
+    "revision": "6bdcf8947c965f00bdb0",
+    "url": "/Chess-ThomasG/static/js/2.05101e02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Chess-ThomasG/static/js/2.05101e02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e136e7d520013b7c2481",
+    "url": "/Chess-ThomasG/static/js/main.a70c85db.chunk.js"
+  },
+  {
+    "revision": "d84ddcddc9d29d282953",
+    "url": "/Chess-ThomasG/static/js/runtime-main.a9d246d5.js"
+  }
+]);
